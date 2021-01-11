9febc97f52060bd0f2316a4d5ed8819c2f2e8360
Nizami <nizami.zamanov@gmail.com>
2021-01-11 14:48:45 +0100

    v2

---
+++ gotler.js
@@ -0,0 +1,7 @@
+const GOTLER = [
+    "obama", "Obama", "Beiden", "beiden", "Amerika", "amerika", "usa", "USA"
+];
+
+const BOMBALAR = [
+    "Nizami", "nizami", "Putin", "putin", "Ilham", "ilham"
+];
--- index.js
+++ index.js
@@ -1,6 +1,45 @@
+const GOTLER = [
+    "obama", "Obama", "Beiden", "beiden", "Hillary", "hillary", "Clinton", "clinton", "Amerika", "amerika", "usa", "USA"
+];
+
+const BOMBALAR = [
+    "Nizami", "nizami", "Putin", "putin", "Ilham", "ilham"
+];
 const {Telegraf} = require('telegraf');
 const bot = new Telegraf('1525462758:AAEbM86corKfiCiQQvp5ZaGv95NMTZFW1MY');
-bot.hears("beiden", (ctx) => {
-    ctx.reply("beiden is an asshole");
+bot.start(ctx => {
+    let result = ctx.botInfo.first_name + ": Ya Allah! Getdiy " + ctx.message.from.first_name;
+    ctx.reply(result);
 });
-bot.launch();
+
+bot.on("text", ctx => {
+    const words = ctx.message.text.split(" ");
+    let result = null;
+    words.forEach(word => {
+        if (GOTLER.indexOf(word) > -1) {
+            const detectedWord = GOTLER[GOTLER.indexOf(word)];
+            result = detectedWord + " gotverendi";
+        } else if (BOMBALAR.indexOf(word) > -1) {
+            const detectedWord = BOMBALAR[BOMBALAR.indexOf(word)];
+            result = detectedWord + " bomba oglandi";
+        }
+    })
+    if (result) {
+        ctx.reply(result);
+    }
+
+});
+// bot.stop( ctx => {
+//     console.log("stop context: ", ctx);
+//     ctx.reply(ctx.botInfo.first_name, ": Ozun sikdir");
+// });
+bot.startPolling();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
+
+// bot.hears("beiden", (ctx) => {
+//     ctx.reply("beiden is an asshole");
+// });
+// bot.launch();
